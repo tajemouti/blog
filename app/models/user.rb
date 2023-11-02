09f6a75d9edfe13dbbfa2c6fe8dfe9347187758a
tajemouti <tajemouti@gmail.com>
2023-11-02 14:16:45 +0100

    Add methods for recent posts and update post counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
+
+  def update_post_counter
+    update(post_counter: posts.count)
+  end
 end
