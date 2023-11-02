efa504d0429b8ef3243846904dd6718fe9802a6b
tajemouti <tajemouti@gmail.com>
2023-11-02 17:11:02 +0100

    Add recent_comments and increment_user_post_counter methods to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_create :increment_user_post_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def increment_user_post_counter
+    author.increment!(:post_counter)
+  end
 end
