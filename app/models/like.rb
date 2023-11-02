4b860fc44520fd78594c30406126b3ae1c4e3df8
tajemouti <tajemouti@gmail.com>
2023-11-02 17:12:52 +0100

    Add increment_post_likes_counter method to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :increment_post_likes_counter
+
+  def increment_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
