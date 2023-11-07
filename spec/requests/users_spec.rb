280df3703f8120b68fd76c703989b321954a2ae8
tajemouti <tajemouti@gmail.com>
2023-11-07 16:02:44 +0100

    Write unit tests for the users show view

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe "Users controller", type: :request do
       expect(response.body).to include("List of all users")
     end
   end
+
+  describe "GET /users/user_id" do
+    before do
+      user = User.create(name: "Example User", post_counter: 0)
+      get user_path(user)
+    end
+
+    it "returns a 200 status code" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the 'show' template" do
+      expect(response).to render_template("show")
+    end
+
+    it "includes 'Specific User' in the response body" do
+      expect(response.body).to include("Specific User")
+    end
+  end
 end
