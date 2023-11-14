c19d856b1cd45bee43524d1f3ca4bb4376ba42ff
tajemouti <tajemouti@gmail.com>
2023-11-14 20:29:17 +0100

    Write unit tests for posts#index view

---
+++ spec/features/post_index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'User Post Index Page' do
+  let!(:user) { User.create(name: 'Ajrass', photo: 'https://avatars.githubusercontent.com/u/130588108?v=4') }
+  let!(:post) do
+    Post.create(author_id: user.id, title: 'Test Post', text: 'Hello world!')
+  end
+  let!(:comment1) { Comment.create(user:, post:, text: 'Hello reviewer!') }
+  let!(:comment2) { Comment.create(user:, post:, text: 'Hope everything is just fine for you!') }
+
+  scenario 'Displays username, profile picture, number of posts and total likes' do
+    visit user_posts_path(user)
+    expect(page).to have_css("img[src='#{user.photo}']")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content("Number of posts: #{user.posts.count}")
+    expect(page).to have_content("Likes: #{post.likes_counter}")
+  end
+
+  scenario 'Displays post title and some of the post body' do
+    visit user_posts_path(user)
+    expect(page).to have_content(post.title)
+    expect(page).to have_content('Hello world!')
+  end
+
+  scenario 'Displays the first comments on a post and total comments' do
+    visit user_posts_path(user)
+    expect(page).to have_content(comment2.text)
+    expect(page).to have_content("Comments: #{post.comments_counter}")
+  end
+end
