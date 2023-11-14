30798571acc79c29da87d89bf0a605fc0930ba71
tajemouti <tajemouti@gmail.com>
2023-11-14 17:54:32 +0100

    Update previous rspec example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is valid with a name and a non-negative posts_counter' do
-    user = User.new(name: 'Tom', post_counter: 0)
+    user = User.new(name: 'Tom', posts_counter: 0)
     expect(user).to be_valid
   end
 
@@ -13,14 +13,14 @@ RSpec.describe User, type: :model do
   end
 
   it 'is not valid with a negative posts_counter' do
-    user = User.new(name: 'Tom', post_counter: -1)
+    user = User.new(name: 'Tom', posts_counter: -1)
     user.valid?
-    expect(user.errors[:post_counter]).to include('must be greater than or equal to 0')
+    expect(user.errors[:posts_counter]).to include('must be greater than or equal to 0')
   end
 
   it 'returns the 3 most recent posts for a user' do
-    user = User.create(name: 'Tom', post_counter: 0)
-    4.times { user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0) }
+    user = User.create(name: 'Tom')
+    4.times { Post.create(author: user, title: 'My Post') }
 
     recent_posts = user.recent_posts
 
